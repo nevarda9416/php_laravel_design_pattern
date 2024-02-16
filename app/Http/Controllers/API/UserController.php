6503aba02dc53bd791bfa33e42ff335af8597600
tudt <tu.dao@logigear.com>
2024-02-16 15:04:35 +0700

    - [Project Architecture]: Dynamic Names of Properties/Methods

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function login(Request $request)
     {
-        
+        $user = User::find(1);
+        // Old: Traditional
+        $user->name = 'Admin';
+        $user->save();
+        // New: Modern
+        $property = 'name';
+        $user->$property = 'Admin';
+        $method = 'save';
+        $user->$method();
     }
 }
