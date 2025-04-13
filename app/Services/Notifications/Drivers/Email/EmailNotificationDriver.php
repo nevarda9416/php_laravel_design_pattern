bae63405d6a1897416fa084170188ba186ab0d4f
tudao <daotientu@gmail.com>
2025-04-13 15:21:43 +0700

    - [Project Architecture]: Dynamic Notification System with Polymorphic Targets (📦 2. Create Channel Drivers)

---
+++ app/Services/Notifications/Drivers/Email/EmailNotificationDriver.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services\Notifications\Drivers\Email;
+
+use App\Services\Notifications\Drivers\NotificationDriverInterface;
+use App\Models\Notification;
+use App\Mail\SendEmail;
+use Illuminate\Support\Facades\Mail;
+
+class EmailNotificationDriver implements NotificationDriverInterface
+{
+    public function send(Notification $notification)
+    {
+        Mail::to($notification->notifiable->email)->send(new SendEmail($notification->data));
+    }
+}
