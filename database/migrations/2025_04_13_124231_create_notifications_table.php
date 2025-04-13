2e28e46443c1b96969339b32f0d38d12e5519108
tudao <daotientu@gmail.com>
2025-04-13 13:33:24 +0700

    - [Project Architecture]: Dynamic Notification System with Polymorphic Targets (🧱 1. Polymorphic Relation: NotificationTarget)

---
+++ database/migrations/2025_04_13_124231_create_notifications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('notifiable'); // user_id + user_type (e.g., User, Team, Organization)
+            $table->string('type'); // e.g. "Slack", "Email"
+            $table->text('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
